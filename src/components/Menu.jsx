92df023366769fac0126b3fda82097e4f603fc57
Igor <victororlov174@gmail.com>
2023-02-18 13:58:57 +0300

    menu redesign

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,14 +4,20 @@ import styles from './Menu.module.css'
 export default function Sidebar() {
   return (
   <div className={styles.side}>
+    <div className={styles.menuTitle}>
+      <span>Posts App</span>
+    </div>
     <nav className={styles.nav}>
       <ul className={styles.navList}>
-      <li><NavLink className={({ isActive }) => (isActive ? styles.activeLink : null)} end to="/">Home</NavLink></li>
+        <li><NavLink className={({ isActive }) => (isActive ? styles.activeLink : null)} end to="/">Home</NavLink></li>
         <li><NavLink className={({ isActive }) => (isActive ? styles.activeLink : null)} end to="/posts">Posts</NavLink></li>
         <li><NavLink className={({ isActive }) => (isActive ? styles.activeLink : null)} to="/users">Users</NavLink></li>
         <li><NavLink className={({ isActive }) => (isActive ? styles.activeLink : null)} to="/comments">Comments</NavLink></li>
       </ul>
     </nav>
+    <div className={styles.menuTitle}>
+      <span>2023</span>
+    </div>
   </div>
   )
 }
